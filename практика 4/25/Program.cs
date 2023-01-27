b786ab621f5ba95b1536237de7be8c840b386e3b
arturasyan777 <arturasyan777@gmail.com>
2023-01-27 18:11:03 +0300

    Домашняя к 4 семинару

---
+++ практика 4/25/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+double result1 (double num, int num1)
+{
+    double B = 1;
+     for (int i = 0; i < num1; i++)
+    {
+        B *= num;
+    }
+    return B;
+}
+
+int minusX(int x)
+
+{
+    if (x < 0)
+    {
+        x = x * (-1);
+        System.Console.WriteLine("Приводим число к натуральному");
+    }
+    return x;
+}
+void decision()
+{
+    System.Console.Write("Введите первое число A : ");
+    double FactorA = Convert.ToSingle(Console.ReadLine());
+    System.Console.Write("Введите второе число B : ");
+    int FactorB = Convert.ToInt32(Console.ReadLine());
+    FactorB = minusX(FactorB);
+
+    System.Console.WriteLine($"A({FactorA}) в степени B({FactorB}) = {result1(FactorA, FactorB)}");
+}
+
+decision();
+
---
+++ практика 4/27/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+
+long minusX(long X)
+{
+   {
+
+    if (X < 0)
+    X = X * -1;
+    return X;
+   }
+}
+long summ (long A)
+{
+    long B = 0;
+    while (A > 0)
+
+    {
+        B= B + A % 10;
+        A= A / 10;
+    
+    }
+    return B;
+
+}
+void decision ()
+{
+    Console.Clear();
+    Console.Write ("Введите число : ");
+    long number = Convert.ToInt64 (Console.ReadLine());
+    number = minusX(number);
+     System.Console.WriteLine($"Сумма цифр  {number} равна {summ(number)}");  
+}
+decision();
