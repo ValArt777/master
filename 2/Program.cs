0de513758e47611a8d5f0107fb962a0b1bc509f5
unknown <arturasyan777@gmail.com>
2023-02-01 18:09:31 +0300

    fdsfds

--- 2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine ("введите трехзначное число: ");
-
-int num1 = int.Parse(Console.ReadLine ()!);
-
-int a = num1 / 10 % 10;
-
-Console.WriteLine ($"Введенное число {num1} -> Вторая цифра {a}");
---
+++ test.md
@@ -0,0 +1,1 @@
+adfdfdasfdasasdsda
